9f1522e802fa4d6e9a244f0433024c8bb30816c7
radiopushka <48099825+radiopushka@users.noreply.github.com>
2022-08-25 12:56:13 -0700

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "psaccess.c"
 #include "definitions.h"
 
+
 int main(int argn,char** args){
         set_filter("kworker");
         if(argn>1){
@@ -25,26 +26,47 @@ int main(int argn,char** args){
          }
         }
 	if(argn==2){
-         char* pathtocmd=filegroup("/proc/",args[1],"/status");
-         printfiledata(pathtocmd);
-         free(pathtocmd);
-         return 0;
+         if(isdigit(args[1][0])){
+          char* pathtocmd=filegroup("/proc/",args[1],"/status");
+          printfiledata(pathtocmd);
+          free(pathtocmd);
+          return 0;
+         }
         }
 	struct dirent *dirinfo;
+        char* notshow="";
+        char* show="";
+        if(argn>2){
+         if(strcmp(args[1],"f")==0){
+          notshow=args[2];
+
+         }
+         if(strcmp(args[1],"r")==0){
+          show=args[2];
+         }
+        }
 	DIR *dir=opendir("/proc");
         if(dir==NULL){ printf("no /proc acccess, terminating"); return 0;}
 	while((dirinfo=readdir(dir))!=NULL){
          char* name=dirinfo->d_name;
 	  if(isdigit(name[0])){
             char* pathtocmd=filegroup("/proc/",name,"/cmdline");
-	    char* pathtostat=filegroup("/proc/",name,"/status");          
-            printf("id: %s | ",name);
-	    printfirstline(pathtostat);
-	    free(pathtostat);
-	    printf(" | launch command: ");
-            printfiledata(pathtocmd);
-            free(pathtocmd);
-            printf("\n");
+            char* pname=getprocessnamefromid(name); 
+            if((strcmp(notshow,"")==0||strstr(pname,notshow))&&strcmp(show,"")==0){
+             printf("id: %s | name: %s",name,pname);
+	     printf(" | launch command: ");
+             printfiledata(pathtocmd);
+             free(pathtocmd);
+             printf("\n");
+            }else if(strcmp(show,"")!=0&&!strstr(pname,show)){
+             printf("id: %s | name: %s",name,pname);
+             printf(" | launch command: ");
+             printfiledata(pathtocmd);
+             free(pathtocmd);
+             printf("\n");
+            }
+            
+            free(pname);
             //printf("type pmmanager h for help option \n");
 	  }
           //printf("type pmmanager h for help option \n");
